17ad1cbf0f58317e00ccf3a59896a3cff3851ebd
Mike Kienenberger <mkienenb@gmail.com>
2022-01-22 16:49:04 -0500

    Replace "!A && !B" w/  ! ("A || B")

--- Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
@@ -14,7 +14,7 @@ class GildedRose {
 	for (int i = 0; i < items.length; i++) {
 	    Item item = items[i];
 
-	    if (!itemMatches(item, ITEM__AGED_BRIE) && !itemMatches(item, ITEM__BACKSTAGE_PASSES)) {
+	    if (! (itemMatches(item, ITEM__AGED_BRIE) || itemMatches(item, ITEM__BACKSTAGE_PASSES)) ) {
 		if (item.quality > 0) {
 		    if (!itemMatches(item, ITEM__SULFURAS)) {
 			decreaseQualityByOne(item);
